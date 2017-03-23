b58908fab0ba4ce1d1fff07954e20eba08ef4e8f
Bryan Lin and Eric Schoettle <bryan@>
2017-03-23 16:42:02 -0700

    change calculator to a one-line form and use regex to calculate the equations

--- js/scripts.js
+++ js/scripts.js
@@ -19,29 +19,35 @@ var exp = function(number1, number2) {
   return number1 ** number2;
 };
 
+
 // front-end logic
 $(document).ready(function() {
   $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
+    var string = $("#input1").val()
+
+    var regexNumber = /[/d]*/
+    var regexOperator = /[+=/*^]/
+
+    var numbers = string.split(regexOperator)
+    var number1 = parseFloat(numbers[0]);
+    var number2 = parseFloat(numbers[1]);
+
+    var operator = regexOperator.exec(string)
+
     var result;
 
-    if (operator === "add") {
+    if (operator[0] === "+") {
       result = add(number1, number2);
-    } else if (operator === "subtract") {
+    } else if (operator[0] === "-") {
       result = subtract(number1, number2);
-    } else if (operator === "multiply") {
+    } else if (operator[0] === "*") {
       result = multiply(number1, number2);
-    } else if (operator === "divide") {
+    } else if (operator[0] === "/") {
       result = divide(number1, number2);
-    } else if (operator === "exp") {
+    } else if (operator[0] === "^") {
       result = exp(number1, number2);
     }
     $("#output").text(result);
   });
 });
-
-
-//regex brain hurt
